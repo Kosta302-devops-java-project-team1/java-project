9c27a049be71a4db92e64074ea0dab36394bef86
donghae <choi7gold@gmail.com>
2025-09-02 10:07:53 +0900

    properties로변경

--- src/main/java/com/project/common/DBManager.java
+++ src/main/java/com/project/common/DBManager.java
@@ -1,61 +1,71 @@
 package main.java.com.project.common;
 /**
  * JDBC를 위한 로드, 연결, 닫기
- * */
+ *
+ */
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class DBManager {
+    private static Properties prop = new Properties();
 
-	/**
-	 * 로드
-	 * */
-	static {
-		try {
-		  Class.forName(DBProperties.DRVIER_NAME);
-		}catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * 연결
-	 * */
-	
-	public static Connection getConnection() throws SQLException{
-		return DriverManager.getConnection(DBProperties.URL,
-				DBProperties.USER_ID, DBProperties.USER_PASS);
-	}
-	
-	/**
-	 * 닫기(DML전용)
-	 * */
-	public static void releaseConnection(Connection con, Statement st) {
-		try {
-			if(st!=null) st.close();
-			if(con!=null)con.close();
-		}catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	/**
-	 * 닫기(select전용)
-	 * */
+    /**
+     * 로드
+     * */
+    static {
+
+        try {
+            prop.load(new FileInputStream("resources/dbInfo.properties"));
+            Class.forName(prop.getProperty("driverName"));
+        } catch (ClassNotFoundException | FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 연결
+     * */
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(prop.getProperty("url"),
+                prop.getProperty("userName"), prop.getProperty("userPass"));
+    }
+
+    /**
+     * 닫기(DML전용)
+     * */
+    public static void releaseConnection(Connection con, Statement st) {
+        try {
+            if (st != null) st.close();
+            if (con != null) con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 닫기(select전용)
+     * */
     public static void releaseConnection(Connection con, Statement st, ResultSet rs) {
-    	try {
-    		if(rs!=null)rs.close();
-    	}catch (SQLException e) {
-			e.printStackTrace();
-		}
-    	releaseConnection(con, st);
-	}
-	
+        try {
+            if (rs != null) rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        releaseConnection(con, st);
+    }
+
 }
 
 
