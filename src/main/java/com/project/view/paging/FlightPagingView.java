745fefcc2278da14e81079c1b1093e67db76b30c
MINYONG PARK <parkminyong22@gmail.com>
2025-09-04 14:29:38 +0900

    #18 항공권 정렬 및 페이징
    - view는 아직 미완성
    - 필터링은 최대한 뒤로 미루기

---
+++ src/main/java/com/project/view/paging/FlightPagingView.java
@@ -0,0 +1,48 @@
+package main.java.com.project.view.paging;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class FlightPagingView {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static <T> void paging(int size, List<T> list, DetailViewHandler<T> handler) {
+        int pageSize = size;
+        int currentPage = 0;
+        int totalPages = (int) Math.ceil((double) list.size() / pageSize);
+
+        while (true) {
+            int start = currentPage * pageSize;
+            int end = Math.min(start + pageSize, list.size());
+
+            System.out.println("\n=== 페이지 " + (currentPage + 1) + " / " + totalPages + " ===");
+            for (int i = start; i < end; i++) {
+                System.out.println((i + 1) + ". " + list.get(i));
+            }
+
+            System.out.println("\n메뉴: [번호 입력] 상세보기 ['['] 이전 [']'] 다음 ['0'] 메인");
+            String input = sc.nextLine();
+
+            if (input.equals("[")) {
+                if (currentPage > 0) currentPage--;
+                else System.out.println("첫 페이지입니다.");
+            } else if (input.equals("]")) {
+                if (currentPage < totalPages - 1) currentPage++;
+                else System.out.println("마지막 페이지입니다.");
+            } else if (input.equals("0")) {
+                return;
+            } else {
+                try {
+                    int selected = Integer.parseInt(input);
+                    if (selected >= 1 && selected <= list.size()) {
+                        handler.showDetail(list.get(selected - 1));
+                    } else {
+                        System.out.println("잘못된 번호입니다.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("다시 입력해주세요.");
+                }
+            }
+        }
+    }
+}
